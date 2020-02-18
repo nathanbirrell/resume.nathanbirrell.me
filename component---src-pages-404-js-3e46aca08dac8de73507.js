3085ce776f39a0513709495a5cd086f1122e7f7e
Nathan Birrell <nathanbirrell@gmail.com>
2020-02-18 21:20:11 +1100

    Updates

---
+++ component---src-pages-404-js-3e46aca08dac8de73507.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x9427c64ab85d],{263:function(e,t,l){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var n=l(2),u=a(n),d=l(42),o=a(d),f=function(){return u.default.createElement("div",{style:{}},u.default.createElement("h1",null,u.default.createElement(o.default,{to:"/"},"Page not found, go ",u.default.createElement("span",{title:"home"},"🏠"))),u.default.createElement("p",null,"This page does not exist."))};t.default=f,e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-404-js-3e46aca08dac8de73507.js.map
--- src/components/Separator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Separator = () => (
-  <span>
-    {' | '}
-  </span>
-)
-
-export default Separator
